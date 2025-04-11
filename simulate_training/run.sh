4aa22e1079aef8068f73715912e42a38fb47664c
NikolayBlagoev <workerthreadllc@gmail.com>
2025-04-11 14:20:43 +0300

    small change to run

--- simulate_training/run.sh
+++ simulate_training/run.sh
@@ -2,14 +2,13 @@ pkill python
 git pull
 
 CHECKPOINT_MODE=$1
-device=$2
-world_size=$3
-h_failure_probability=$4
-config=$5
+world_size=$2
+h_failure_probability=$3
+config=$4
 for ((i=0; i<$world_size; i=i+1))
 do
     touch "out$i.txt"
-    (sleep 1; python -u "convergence_training.py" $CHECKPOINT_MODE $device $i $world_size $h_failure_probability $config>"out$i.txt")&
+    (sleep 1; python -u "convergence_training.py" $CHECKPOINT_MODE "cuda:$i" $i $world_size $h_failure_probability $config>"out$i.txt")&
 
 
 done
