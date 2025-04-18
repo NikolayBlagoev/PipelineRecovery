f1b7d3367cb8ebc9863838e79cf8fa0c24793f0c
NikolayBlagoev <workerthreadllc@gmail.com>
2025-04-18 15:23:14 +0300

    adjust ps

--- simulate_training/convergence_training.py
+++ simulate_training/convergence_training.py
@@ -160,14 +160,13 @@ for _ in range(mb_count):
                     
         x = stages[0].forward_end(x)
 t1 = (time() - t1)
-# 60 MB/s or ~480Mb/s
-t1 += len(stages)*mb_size / (0.06*1024**3) # in reality you will do multiple waves
-t1 += len(stages)*0.3 # account for trailing and other delays
-t1 = t1 * 3 # in reality you will do multiple waves
+# 100 MB/s or ~800Mb/s
+t1 += len(stages)*mb_size / (0.1*1024**3) # in reality you will do multiple waves
+t1 = t1 * 2 # in reality you will do multiple waves
 print("time for F to B",t1 * 2.5) # backwards is a bit slower
-print("time for dp", (3*world_size - 1) * sum(vls[-1][1]) * 8 / (0.06*1024**3))
-total_time = t1 * 2.5 + (3*world_size - 1) * sum(vls[-1][1]) * 8 / (0.06*1024**3)
-total_time *=  1.1 # synchronisation
+print("time for dp", (2*world_size - 1) * sum(vls[-1][1]) * 8 / (0.1*1024**3))
+total_time = t1 * 2.5 + (2*world_size - 1) * sum(vls[-1][1]) * 8 / (0.1*1024**3)
+total_time *=  1 # synchronisation
 print("total time per iteration ", total_time)
 iterations_per_h = 60*60 / total_time 
 print(60*60 / total_time, 100 - h_failure_probability)
