ba3ae41b8313cc56a012c243edbd353fec0e2786
NikolayBlagoev <workerthreadllc@gmail.com>
2025-04-25 13:23:37 +0300

    fix run script

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ do
     touch "log$i.txt"
     touch "ERROR_LOG_$i.txt"
     touch "out$i.txt"
-    (sleep 1; python -u "trainer.py" $i $2 $3 >"out$i.txt") &
+    (sleep 1; python -u "trainer.py" $i $3 $4 >"out$i.txt") &
 
 
 done
